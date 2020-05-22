ac86f3ac58deaaf4991721ef5771a1a681d97241
hsudale <hsudale@gmail.com>
2020-05-22 11:26:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+//class App extends Component {
+//  render() {
+//    return (
+//    );
+//  }
+//}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+mySubmitHandler = (event) => {
+  event.preventDefault();
+  //let age = this.state.age;
+  //if (!Number(age)) {
+  //  alert("Your age must be a number");
+  //}
+}
+
+myChangeHandler = (event) => {
+  let nam = event.target.name;
+  let val = event.target.value;
+  this.setState({[nam]: val});
+}
+
+//export default function FormPropsTextFields() {
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ---  Learn React XXX
-          </a>
-        </header>
+  const classes = useStyles();
+  return (
+    <form className={classes.root} noValidate autoComplete="off"
+          onSubmit={this.mySubmitHandler}>
+      <div>
+        <TextField required id="fname" label="First Name: " defaultValue="" />
+        <TextField required id="lname" label="Last Name: " defaultValue="" />
+        <TextField required id="email" label="Send-to Email: " defaultValue="" />
+        <br/>
+        <br/>
+        <TextField
+          id="message"
+          label="Message: "
+          multiline
+          rows={5}
+          defaultValue=" "
+          variant="outlined"
+        />
+        <br/>
+        <br/>
+        <Button 
+            variant="contained" 
+            type="submit"
+            size="large" 
+            color="primary" 
+            className={classes.margin}>
+            Submit
+        </Button>
       </div>
-    );
+    </form>
+  );
   }
 }
 
