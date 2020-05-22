82c7c17f542e212085248f782c8f117b3b0a27eb
hsudale <hsudale@gmail.com>
2020-05-22 10:15:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,135 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-import * as serviceWorker from './serviceWorker';
-//ReactDOM.render(<App />, document.getElementById('root'));
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-//
-//** customized code starts 
-//
-import { Form, Field } from 'react-final-form';
-import { TextField, Checkbox, Radio, Select } from 'final-form-material-ui';
-import {
-  Typography,
-  Paper,
-  Link,
-  Grid,
-  Button,
-  CssBaseline,
-  RadioGroup,
-  FormLabel,
-  MenuItem,
-  FormGroup,
-  FormControl,
-  FormControlLabel,
-} from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
-const onSubmit = async values => {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-  await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
-};
-const validate = values => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  }
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  }
-  if (!values.email) {
-    errors.email = 'Required';
-  }
-  return errors;
-};
-
-function App() {
-  return (
-    <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
-      <CssBaseline />
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
-        Email Someone Form
-      </Typography>
-      <Form
-        onSubmit={onSubmit}
-        initialValues={{ employed: true, stooge: 'larry' }}
-        validate={validate}
-        render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
-            <Paper style={{ padding: 16 }}>
-              <Grid container alignItems="flex-start" spacing={2}>
-                <Grid item xs={6}>
-                  <Field
-                    fullWidth
-                    required
-                    name="firstName"
-                    component={TextField}
-                    type="text"
-                    label="First Name"
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <Field
-                    fullWidth
-                    required
-                    name="lastName"
-                    component={TextField}
-                    type="text"
-                    label="Last Name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    name="Email"
-                    fullWidth
-                    required
-                    component={TextField}
-                    type="email"
-                    label="Send to Email (ex. somethingn@domain)"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    fullWidth
-                    name="msg"
-                    component={TextField}
-                    multiline
-                    label="Message to send"
-                  />
-                </Grid>
-                <Grid item style={{ marginTop: 16 }}>
-                  <Button
-                    type="button"
-                    variant="contained"
-                    onClick={reset}
-                    disabled={submitting || pristine}
-                  >
-                    Reset
-                  </Button>
-                </Grid>
-                <Grid item style={{ marginTop: 16 }}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    disabled={submitting}
-                  >
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </Paper>
-          </form>
-        )}
-      />
-    </div>
-  );
+mySubmitHandler = (event) => {
+  event.preventDefault();
+  //let age = this.state.age;
+  //if (!Number(age)) {
+  //  alert("Your age must be a number");
+  //}
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+myChangeHandler = (event) => {
+  let nam = event.target.name;
+  let val = event.target.value;
+  this.setState({[nam]: val});
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default function FormPropsTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off"
+          onSubmit={this.mySubmitHandler}>
+      <div>
+        <TextField required id="fname" label="First Name: " defaultValue="" />
+        <TextField required id="lname" label="Last Name: " defaultValue="" />
+        <TextField required id="email" label="Send-to Email: " defaultValue="" />
+        <br/>
+        <br/>
+        <TextField
+          id="message"
+          label="Message: "
+          multiline
+          rows={5}
+          defaultValue=" "
+          variant="outlined"
+        />
+        <br/>
+        <br/>
+        <Button 
+            variant="contained" 
+            type="submit"
+            size="large" 
+            color="primary" 
+            className={classes.margin}>
+            Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
