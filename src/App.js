3853800c7aad0eea43e467a3f31a7560b7a4c38e
hsudale <hsudale@gmail.com>
2020-05-22 12:13:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,52 +3,23 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
-//class App extends Component {
-//  render() {
-//    return (
-//    );
-//  }
-//}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
-
-mySubmitHandler = (event) => {
-  event.preventDefault();
-  let age = this.state.age;
-  //if (!Number(age)) {
-  //  alert("Your age must be a number");
-  //}
-}
-
-myChangeHandler = (event) => {
-  let nam = event.target.name;
-  let val = event.target.value;
-  this.setState({[nam]: val});
-}
+Amplify.configure(aws_exports);
 
 //export default function FormPropsTextFields() {
 class App extends Component {
   render() {
-  const classes = useStyles();
   return (
-    <form className={classes.root} noValidate autoComplete="off"
-          onSubmit={this.mySubmitHandler}>
+    <form noValidate autoComplete="off">
       <div>
         <TextField required id="fname" label="First Name: " defaultValue="" />
+        <br/>
         <TextField required id="lname" label="Last Name: " defaultValue="" />
+        <br/>
         <TextField required id="email" label="Send-to Email: " defaultValue="" />
+        <br/>
+        <br/>
         <br/>
         <br/>
         <TextField
@@ -58,15 +29,20 @@ class App extends Component {
           rows={5}
           defaultValue=" "
           variant="outlined"
-        />
+        />        <br/>
+
         <br/>
         <br/>
-        <Button 
+        <Butt        <br/>
+        <br/>
+        <br/>
+on 
             variant="contained" 
+        <br/>
+        <br/>
             type="submit"
             size="large" 
-            color="primary" 
-            className={classes.margin}>
+            color="primary" >
             Submit
         </Button>
       </div>
